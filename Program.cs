c14e06f9c9ce8709fc7d946c0ff9123e2a902623
Ronald W. (Bill) Butts, Jr <rwbutts@gmail.com>
2023-11-15 04:13:05 -0500

    refactor

---
+++ Program.cs
@@ -0,0 +1,75 @@
+using System.Diagnostics;
+using WordlessAPI;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+
+app.MapGet("/healthcheck", () => "Alive.");
+
+app.MapGet("/api/randomword",  () =>{ 
+
+          var R = Words.randomWord( );
+          return new RandomWordResponse( R.Item1, R.Item2 );
+     }
+);
+
+app.MapGet("/api/checkword/{word}",  ( string word ) =>
+     new WordExistsResponse( Words.wordExists( word ) )
+);
+
+app.MapGet("/api/getword/{daysago}",  ( int daysago ) => { 
+
+          var R = Words.todaysWord( daysago );
+          return new RandomWordResponse( R.Item1, R.Item2 );
+     }
+);
+
+app.MapPost("/api/querymatchcount",  (QueryMatchCountRequest request) =>
+{
+     int count = Filter.findMatches( Words.wordList, request.answer, new List<String>(request.guesses));
+ 
+    return Results.Ok( new QueryMatchCountResponse(count));
+});
+
+app.UseSwaggerUI();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.Run();
+
+public record RandomWordResponse( int index, string word );
+
+public record WordExistsResponse( bool exists );
+
+public class QueryMatchCountRequest
+{
+     public string answer { get; set; } 
+     public string [] guesses { get; set; } 
+
+     public QueryMatchCountRequest()
+     {
+          this.answer = "";
+          this.guesses = new string[] {};
+     }
+}
+ public class QueryMatchCountResponse
+{
+     public int count { get; set; }
+
+     public QueryMatchCountResponse( int count )
+     {
+          this.count = count;
+     }
+}
---
+++ filter.cs
@@ -0,0 +1,74 @@
+
+namespace WordlessAPI {
+     static class Filter {
+          static bool matchesYellow( string testWord, string guessChar, int charIndex )
+          {
+               return !matchesGreen( testWord, guessChar, charIndex) && testWord.Contains(guessChar);
+          }
+
+          static bool matchesGreen( string testWord, string guessChar, int charIndex )
+          {
+               return guessChar == testWord.Substring( charIndex, 1 );
+          }
+
+          static bool isMatch( string testWord, string answer, string guess )
+          {
+
+                    for( int i = 0; i < testWord.Length; i++ )
+                    {
+                         string c = guess.Substring( i, 1 );
+                         if( matchesGreen( answer, c, i ) )
+                         {
+                              // guess character matched green against answer, 
+                              // but doesnt match green for testWord letter: eliminate
+                              if( !matchesGreen( testWord, c, i ) )
+                                   return false;
+                         }
+                         else if( matchesYellow ( answer, c, i ) )
+                         {
+                              // guess matches a letter elsewhere in answer, 
+                              // does it do so in testWord?
+                              if( !matchesYellow( testWord, c, i ) )
+                                   return false;
+                         }
+                         else
+                         {
+                              // letter c is not in answer. Is it in the Candidate? 
+                              // Reject if it is.
+                              if( testWord.Contains( c ) )
+                                   return false;
+                         }
+                    }
+                    return true;
+          }
+
+          public static int findMatches( string[] candidates, string answer, List<string> guesses )
+          {
+               int matchCount = 0;
+
+               foreach( var candidate in candidates )
+               {
+                    bool finalIsMatch = true;
+                    foreach( var guess in guesses )
+                    {
+                         if( !isMatch( candidate, answer, guess ) )
+                         {
+                              finalIsMatch = false;
+                              break;
+                         }
+                    }
+
+                    if( finalIsMatch )
+                    {
+                         matchCount ++;
+                    }
+               }
+
+               return matchCount;
+
+          }
+
+     }
+
+
+}
